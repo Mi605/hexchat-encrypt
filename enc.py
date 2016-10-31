6e2c44057528f66cda8a33d4d81f3595a47b2395
Robert <robert@Roberts-Mac-mini.local>
2016-10-31 01:37:32 +0100

    ..

--- enc.py
+++ enc.py
@@ -9,17 +9,16 @@ __module_version__ = "1.0"
 __module_description__ = "XChat symmetric encryption" 
 
 PROCESSING = False
-KEYFILE = None
+PASSFILE = None
 CHANNELS = set()
-COLORS = { 'GREEN': "\x0303", 
-		   'RED': "\x0304"	}
+COLORS = { 'GREEN': "\x0303", 'RED': "\x0304" }
 			
 def channelServer(ctx):
 	return (ctx.get_info('channel'), ctx.get_info('server'))
 
 def encrypt(plaintext):
 	process = subprocess.Popen(
-		["openssl","enc","-aes-256-cbc","-e","-a","-A","-pass","file:"+KEYFILE],
+		["openssl","enc","-aes-256-cbc","-e","-salt","-a","-A","-pass","file:" + PASSFILE],
 		stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	stdout,stderr = process.communicate(plaintext)      
 	if process.returncode == 0: return stdout
@@ -27,7 +26,7 @@ def encrypt(plaintext):
 
 def decrypt(cryptogram):
 	process = subprocess.Popen(
-		["openssl","enc","-aes-256-cbc","-d","-a","-A","-pass","file:"+KEYFILE],
+		["openssl","enc","-aes-256-cbc","-d","-a","-A","-pass","file:" + PASSFILE],
 		stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	stdout,stderr = process.communicate(cryptogram)
 	if process.returncode == 0: return stdout
@@ -39,12 +38,14 @@ def send(word, word_eol, userdata):
 		message = word_eol[0]
 		try:
 			xchat.command('PRIVMSG %s :%s' % 
-				(ctx.get_info('channel'), "ENC:" + encrypt(message)))
+				(ctx.get_info('channel'), "XCHATENC:" 
+					+ encrypt(message)))
 			xchat.emit_print('Your Message', 
 				xchat.get_info('nick'), message)
 			return xchat.EAT_XCHAT
 		except Exception as e:
-			ctx.prnt(COLORS['RED'] + "Could not encrypt!" + " " + str(e))
+			ctx.prnt(COLORS['RED'] + 
+				"Could not encrypt!")
 			return xchat.EAT_ALL
 	return xchat.EAT_NONE
 
@@ -54,15 +55,17 @@ def receive(word, word_eol, userdata):
 		return xchat.EAT_NONE
 	sender,message = word[0],word[1]
 	ctx = xchat.get_context()
-	if message[:4] == "ENC:":
+	if message[:9] == "XCHATENC:":
 		try:
-			plaintext = decrypt(message[4:])
+			plaintext = decrypt(message[9:])
 			PROCESSING = True
 			ctx.emit_print('Private Message to Dialog', 
 				sender, COLORS['GREEN'] + plaintext)
 			PROCESSING = False
 			return xchat.EAT_XCHAT
 		except Exception as e:
+			ctx.prnt(COLORS['RED'] + 
+				"Could not decrypt!")
 			return xchat.EAT_NONE
 	return xchat.EAT_NONE
 			
@@ -73,17 +76,11 @@ def info(ctx):
 	else:
 		ctx.prnt(COLORS['RED'] + 
 		"Outgoing encryption is disabled for this channel")
-	ctx.prnt("Keypath: " + 
-		("No keyfile found" if KEYFILE is None else KEYFILE))
 	return xchat.EAT_ALL
 
 def enable(ctx):
-	if KEYFILE is not None:
-		CHANNELS.add(channelServer(ctx))
-		ctx.prnt(COLORS['GREEN'] + "Encryption enabled")
-	else:
-		ctx.prnt(COLORS['RED'] + 
-			"Encryption could not be enabled because no keyfile exists.")
+	CHANNELS.add(channelServer(ctx))
+	ctx.prnt(COLORS['GREEN'] + "Encryption enabled")
 	return xchat.EAT_ALL
 
 def disable(ctx):
@@ -94,27 +91,28 @@ def disable(ctx):
 
 def enc(word,word_eol,userdata):
 	ctx = xchat.get_context()
-	arg = word[1]
-	if arg == "enable":
-		enable(ctx)
-	elif arg == "disable":
-		disable(ctx)
-	elif arg == "info":
-		info(ctx)
+	if len(word) > 1:
+		arg = word[1]
+		if arg == "enable":
+			enable(ctx)
+		elif arg == "disable":
+			disable(ctx)
+		elif arg == "info":
+			info(ctx)
 	return xchat.EAT_ALL
 
-def loadKeyFile(path):
-	keypath = path + "/key.bin"
-	if os.path.isfile(keypath):
-		global KEYFILE
-		KEYFILE = keypath
-		print(COLORS['GREEN'] + "Key: " + keypath + " loaded!")
-	else: 
-		print(COLORS['RED'] + "No keyfile found in " + path)
+def init():
+	filepath = xchat.get_info('xchatdir') + "/pass.key"
+	if os.path.isfile(filepath):
+		global PASSFILE
+		PASSFILE = filepath
+		xchat.prnt(COLORS['GREEN'] + PASSFILE + " loaded!")
+		xchat.hook_command('', send)
+		xchat.hook_command('enc',enc)
+		xchat.hook_print('Private Message to Dialog', receive)	
+	else:
+		xchat.prnt(COLORS['RED'] + "No password file found! Add " + 
+			filepath + " and reload script")
 
-
-xchat.hook_command('', send)
-xchat.hook_command('enc',enc)
-xchat.hook_print('Private Message to Dialog', receive)
-loadKeyFile(xchat.get_info('xchatdir'))
+init()
 
