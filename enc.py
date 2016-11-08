aa757f915e60eb750b0857539645fcddce359e56
Robert <robert@Roberts-Mac-mini.local>
2016-11-08 18:43:14 +0100

    ..

--- enc.py
+++ enc.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # https://hexchat.readthedocs.io/en/latest/script_python.html
 
@@ -11,30 +11,38 @@ __module_name__ = "hexchat-encrypt"
 __module_version__ = "1.0" 
 __module_description__ = "hexchat symmetric encryption" 
 
-COLORS = { 'GREEN': "\x0303", 'RED': "\x0304" }
+COLORS = { 'GREEN': "\x0303", 'RED': "\x0304", 'BOLD': "\002" }
 PROCESSING = False
 PASSFILE = None
 CHANNELS = set()
 DEBUG = False
 MCHARSIZE = 330
-			
+
+# Return channel and servername from the specified context
 def channelServer(ctxt):
 	return (ctxt.get_info('channel'), ctxt.get_info('server'))
 
+# Return 'message' as green text
 def textPos(message):
 	return COLORS['GREEN'] + message
 
+# Return 'message' as red text
 def textNeg(message):
 	return COLORS['RED'] + message
 
+def textBold(message):
+	return COLORS['BOLD'] + message
+
+# Encrypt 'plaintext' through the openssl commandline client
 def encrypt(plaintext):
 	process = subprocess.Popen(
 		["openssl","enc","-aes-256-cbc","-e","-a","-A","-pass","file:" + PASSFILE],
 		stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	stdout,stderr = process.communicate(plaintext)      
-	if process.returncode == 0: return stdout
+	if process.returncode == 0: return stdout # Return as base64
 	raise Exception(stderr)
 
+# Decrypt 'cryptogram' through the openssl commandline client
 def decrypt(cryptogram):
 	process = subprocess.Popen(
 		["openssl","enc","-aes-256-cbc","-d","-a","-A","-pass","file:" + PASSFILE],
@@ -42,16 +50,25 @@ def decrypt(cryptogram):
 	stdout,stderr = process.communicate(cryptogram)
 	if process.returncode == 0: return stdout
 	raise Exception(stderr)
-		
+
+# Invoked everytime a user sends a message
 def send(word, word_eol, userdata):
 	ctxt = hexchat.get_context()
-	if channelServer(ctxt) in CHANNELS:
+	""" Only encrypt outgoing message if channel/server 
+	is added to CHANNELS list """
+	if channelServer(ctxt) in CHANNELS: 
 		message = word_eol[0]
 		try:
+			""" To avoid the encrypted text gets cut off during transmission
+			encrypt and send the message in chunks of MCHARSIZE character-
+			length . """
 			for x in range(0,len(message),MCHARSIZE): 
+				""" To mark the message as encrypted, 'HEXCHATENC:' is concatenated
+				to the encrypted message. """
 				hexchat.command('PRIVMSG %s :%s' % 
 					(ctxt.get_info('channel'), "HEXCHATENC:" 
 						+ encrypt(message[x:x+MCHARSIZE])))
+				
 			hexchat.emit_print('Your Message', 
 				hexchat.get_info('nick'), textPos(message))
 			return hexchat.EAT_HEXCHAT
@@ -61,12 +78,15 @@ def send(word, word_eol, userdata):
 			return hexchat.EAT_ALL
 	return hexchat.EAT_NONE
 
+# Invoked everytime a message is received in a private dialog
 def receive(word, word_eol, userdata):
 	global PROCESSING
 	if PROCESSING:
 		return hexchat.EAT_NONE
 	sender,message = word[0],word[1]
 	ctxt = hexchat.get_context()
+	""" If the first characters of the received message 
+	is the word 'HEXCHATENC:' the text is probably encrypted """
 	if message[:11] == "HEXCHATENC:":
 		try:
 			plaintext = decrypt(message[11:])
@@ -80,39 +100,44 @@ def receive(word, word_eol, userdata):
 			if DEBUG: ctxt.prnt(str(e))
 			return hexchat.EAT_NONE
 	return hexchat.EAT_NONE
-			
+
+# Print info about encryption/debug/passfile			
 def info(ctxt):
+	ctxt.prnt(textBold("------------ Info ------------"))
+	ctxt.prnt("* Passfile: " + PASSFILE )
+	ctxt.prnt("* Debug enabled" if DEBUG else "* Debug disabled")
 	if channelServer(ctxt) in CHANNELS:
-		ctxt.prnt(textPos("Outgoing encryption enabled"))
+		ctxt.prnt("* Outgoing encryption enabled")
 	else:
-		ctxt.prnt(textNeg("Outgoing encryption disabled"))
+		ctxt.prnt("* Outgoing encryption disabled")
 	return hexchat.EAT_ALL
 
+# Enable outgoing encryption for current channel
 def enable(ctxt):
 	CHANNELS.add(channelServer(ctxt))
 	ctxt.prnt(textPos("Encryption enabled"))
 	return hexchat.EAT_ALL
 
+# Disable outgoing encryption for current channel
 def disable(ctxt):
 	if channelServer(ctxt) in CHANNELS:
 		CHANNELS.remove(channelServer(ctxt))
 		ctxt.prnt(textNeg("Encryption disabled"))
 	return hexchat.EAT_ALL
 
+# Enabled/disable verbose error messages
 def debug(ctxt):
 	global DEBUG
-	if DEBUG:
-		DEBUG = False
-		ctxt.prnt("Debug disabled")
-	else:
-		DEBUG = True
-		ctxt.prnt("Debug enabled")
+	DEBUG = DEBUG ^ 1
+	ctxt.prnt("Debug enabled" 
+		if DEBUG else "Debug disabled")
 	return hexchat.EAT_ALL
 
 def readConf(section,option):
 	confFilePath = hexchat.get_info('configdir') + '/enc.conf'
 	if not os.path.isfile(confFilePath): 
-		raise IOError(confFilePath)
+		# Conf-file non-existent
+		raise IOError(confFilePath) 
 	config = ConfigParser.ConfigParser()
 	config.read(confFilePath)
 	return config.get(section,option)
@@ -133,6 +158,7 @@ def enc(word,word_eol,userdata):
 		pass
 	return hexchat.EAT_ALL
 
+# Initialization function
 def init():
 	try:
 		filepath = readConf('PASSFILE','path')
