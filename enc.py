277ef1a8a97d92c2508bf90abf2855910974afbd
Robert <robert@Roberts-Mac-mini.local>
2016-11-08 18:59:49 +0100

    ..

--- enc.py
+++ enc.py
@@ -11,29 +11,29 @@ __module_name__ = "hexchat-encrypt"
 __module_version__ = "1.0" 
 __module_description__ = "hexchat symmetric encryption" 
 
-COLORS = { 'GREEN': "\x0303", 'RED': "\x0304", 'BOLD': "\002" }
 PROCESSING = False
 PASSFILE = None
 CHANNELS = set()
 DEBUG = False
 MCHARSIZE = 330
 
-# Return channel and servername from the specified context
+""" Return channel and servername from the specified context """
 def channelServer(ctxt):
 	return (ctxt.get_info('channel'), ctxt.get_info('server'))
 
-# Return 'message' as green text
+""" Return 'message' as green text """
 def textPos(message):
-	return COLORS['GREEN'] + message
+	return "\x0303" + message
 
-# Return 'message' as red text
+""" Return 'message' as red text """
 def textNeg(message):
-	return COLORS['RED'] + message
+	return "\x0304" + message
 
+""" Return 'message' as bold text """
 def textBold(message):
-	return COLORS['BOLD'] + message
+	return "\002" + message
 
-# Encrypt 'plaintext' through the openssl commandline client
+""" Encrypt 'plaintext' through the openssl command line client """
 def encrypt(plaintext):
 	process = subprocess.Popen(
 		["openssl","enc","-aes-256-cbc","-e","-a","-A","-pass","file:" + PASSFILE],
@@ -42,7 +42,7 @@ def encrypt(plaintext):
 	if process.returncode == 0: return stdout # Return as base64
 	raise Exception(stderr)
 
-# Decrypt 'cryptogram' through the openssl commandline client
+""" Decrypt 'cryptogram' through the openssl command line client """
 def decrypt(cryptogram):
 	process = subprocess.Popen(
 		["openssl","enc","-aes-256-cbc","-d","-a","-A","-pass","file:" + PASSFILE],
@@ -51,7 +51,7 @@ def decrypt(cryptogram):
 	if process.returncode == 0: return stdout
 	raise Exception(stderr)
 
-# Invoked everytime a user sends a message
+""" Invoked every time a user sends a message """
 def send(word, word_eol, userdata):
 	ctxt = hexchat.get_context()
 	""" Only encrypt outgoing message if channel/server 
@@ -68,7 +68,7 @@ def send(word, word_eol, userdata):
 				hexchat.command('PRIVMSG %s :%s' % 
 					(ctxt.get_info('channel'), "HEXCHATENC:" 
 						+ encrypt(message[x:x+MCHARSIZE])))
-				
+
 			hexchat.emit_print('Your Message', 
 				hexchat.get_info('nick'), textPos(message))
 			return hexchat.EAT_HEXCHAT
@@ -78,7 +78,7 @@ def send(word, word_eol, userdata):
 			return hexchat.EAT_ALL
 	return hexchat.EAT_NONE
 
-# Invoked everytime a message is received in a private dialog
+""" Invoked every time a message is received in a private dialog """
 def receive(word, word_eol, userdata):
 	global PROCESSING
 	if PROCESSING:
@@ -101,7 +101,7 @@ def receive(word, word_eol, userdata):
 			return hexchat.EAT_NONE
 	return hexchat.EAT_NONE
 
-# Print info about encryption/debug/passfile			
+""" Print info about encryption/debug/passfile """
 def info(ctxt):
 	ctxt.prnt(textBold("------------ Info ------------"))
 	ctxt.prnt("* Passfile: " + PASSFILE )
@@ -112,20 +112,20 @@ def info(ctxt):
 		ctxt.prnt("* Outgoing encryption disabled")
 	return hexchat.EAT_ALL
 
-# Enable outgoing encryption for current channel
+""" Enable outgoing encryption for current channel """
 def enable(ctxt):
 	CHANNELS.add(channelServer(ctxt))
 	ctxt.prnt(textPos("Encryption enabled"))
 	return hexchat.EAT_ALL
 
-# Disable outgoing encryption for current channel
+""" Disable outgoing encryption for current channel """
 def disable(ctxt):
 	if channelServer(ctxt) in CHANNELS:
 		CHANNELS.remove(channelServer(ctxt))
 		ctxt.prnt(textNeg("Encryption disabled"))
 	return hexchat.EAT_ALL
 
-# Enabled/disable verbose error messages
+""" Enabled/disable verbose error messages """
 def debug(ctxt):
 	global DEBUG
 	DEBUG = DEBUG ^ 1
@@ -133,10 +133,10 @@ def debug(ctxt):
 		if DEBUG else "Debug disabled")
 	return hexchat.EAT_ALL
 
+""" Try to read the config file (enc.conf) in ~/.config/hexchat"""
 def readConf(section,option):
 	confFilePath = hexchat.get_info('configdir') + '/enc.conf'
-	if not os.path.isfile(confFilePath): 
-		# Conf-file non-existent
+	if not os.path.isfile(confFilePath): # No conf file
 		raise IOError(confFilePath) 
 	config = ConfigParser.ConfigParser()
 	config.read(confFilePath)
@@ -158,7 +158,7 @@ def enc(word,word_eol,userdata):
 		pass
 	return hexchat.EAT_ALL
 
-# Initialization function
+""" Initialization function """
 def init():
 	try:
 		filepath = readConf('PASSFILE','path')
