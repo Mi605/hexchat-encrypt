ece40848964922bda2d554fbdbaa2b691dde1fbf
Robert <robert@Roberts-Mac-mini.local>
2016-11-10 17:30:58 +0100

    ..

--- enc.py
+++ enc.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# The MIT License (MIT)
+# 
+# Copyright (c) [2016] [Robert Kvant]
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE
+
+# HexChat Python Interface Documentation
 # https://hexchat.readthedocs.io/en/latest/script_python.html
 
 import hexchat
@@ -12,7 +36,7 @@ __module_description__ = "hexchat symmetric encryption"
 
 PROCESSING = False
 PASSFILE = "/home/user/pass.key" # Path to key 
-CHANNELS = set()
+DIALOGS = set()
 DEBUG = False
 MCHARSIZE = 330
 
@@ -38,8 +62,8 @@ def decrypt(cryptogram):
 def send(word, word_eol, userdata):
 	ctxt = hexchat.get_context()
 	""" Only encrypt outgoing message if channel/server 
-	is added to CHANNELS list """
-	if channelServer(ctxt) in CHANNELS: 
+	is added to DIALOGS list """
+	if channelServer(ctxt) in DIALOGS: 
 		message = word_eol[0]
 		try:
 			""" To avoid the encrypted text gets cut off during transmission
@@ -87,23 +111,65 @@ def receive(word, word_eol, userdata):
 
 """ Enable outgoing encryption for current channel """
 def enable(ctxt):
-	CHANNELS.add(channelServer(ctxt))
-	ctxt.prnt(textPos("Encryption enabled"))
+	if isDialog(ctxt):
+		DIALOGS.add(channelServer(ctxt))
+		ctxt.prnt(textPos("Encryption enabled"))
+	else:
+		ctxt.prnt(textNeg("Encryption can only be enabled" + 
+			" on private dialog windows"))
 	return hexchat.EAT_ALL
 
 """ Disable outgoing encryption for current channel """
 def disable(ctxt):
-	if channelServer(ctxt) in CHANNELS:
-		CHANNELS.remove(channelServer(ctxt))
-		ctxt.prnt(textNeg("Encryption disabled"))
+	if channelServer(ctxt) in DIALOGS:
+		DIALOGS.remove(channelServer(ctxt))
+	ctxt.prnt(textNeg("Encryption disabled"))
 	return hexchat.EAT_ALL
 
-""" Enabled/disable verbose error messages """
+""" Enable/disable verbose error messages """
 def debug(ctxt):
 	global DEBUG
 	DEBUG = DEBUG ^ 1
+	ctxt.prnt("Debug enabled" if DEBUG
+		else "Debug disabled")
 	return hexchat.EAT_ALL
 
+""" Return channel and servername from the specified context """
+def channelServer(ctxt):
+	return (ctxt.get_info('channel'), ctxt.get_info('server'))
+
+""" Returns true if current context is a dialog window """
+def isDialog(ctxt):
+	return [x.type for x in ctxt.get_list('channels') 
+		if x.channel == ctxt.get_info('channel')][0] == 3
+
+""" Return 'message' as green text """
+def textPos(message): return "\x0303" + message
+
+""" Return 'message' as red text """
+def textNeg(message): return "\x0304" + message
+
+""" Return 'message' as bold text """
+def textBold(message): return "\002"  + message
+
+""" Print info about encryption/debug/passfile """
+def info(ctxt):
+	ctxt.prnt(textBold("------------------ Info ------------------"))
+	ctxt.prnt("* Passfile: " + PASSFILE )
+	ctxt.prnt("* Debug enabled" if DEBUG else "* Debug disabled")
+	ctxt.prnt("* Encryption enabled" if channelServer(ctxt) in 
+		DIALOGS else "* Encryption disabled")
+	return hexchat.EAT_ALL
+
+""" Print help summary """
+def help(ctxt):
+	ctxt.prnt(textBold("------------------ Help ------------------"))
+	ctxt.prnt(textBold("/enc enable   - Encrypt outgoing messages on current dialog window"))
+	ctxt.prnt(textBold("/enc disable  - Disable encryption of outgoing messages on current dialog window"))
+	ctxt.prnt(textBold("/enc info     - Print status about debug/encryption"))
+	return hexchat.EAT_ALL
+
+""" Handles all /enc command arguments"""
 def enc(word,word_eol,userdata):
 	ctxt = hexchat.get_context()
 	try:
@@ -122,42 +188,11 @@ def enc(word,word_eol,userdata):
 		help(ctxt)
 	return hexchat.EAT_ALL
 
-""" Return channel and servername from the specified context """
-def channelServer(ctxt):
-	return (ctxt.get_info('channel'), ctxt.get_info('server'))
-
-""" Return 'message' as green text """
-def textPos(message): return "\x0303" + message
-
-""" Return 'message' as red text """
-def textNeg(message): return "\x0304" + message
-
-""" Return 'message' as bold text """
-def textBold(message): return "\002"  + message
-
-""" Print info about encryption/debug/passfile """
-def info(ctxt):
-	ctxt.prnt(textBold("------------------ Info ------------------"))
-	ctxt.prnt("* Passfile: " + PASSFILE )
-	ctxt.prnt("* Debug enabled" if DEBUG else "* Debug disabled")
-	if channelServer(ctxt) in CHANNELS:
-		ctxt.prnt("* Encryption enabled")
-	else:
-		ctxt.prnt("* Encryption disabled")
-	return hexchat.EAT_ALL
-
-""" Print help summary """
-def help(ctxt):
-	ctxt.prnt(textBold("------------------ Help ------------------"))
-	ctxt.prnt(textBold("/enc enable   - Enable encryption for current dialog"))
-	ctxt.prnt(textBold("/enc disable  - Disable encryption for current dialog"))
-	ctxt.prnt(textBold("/enc info     - Print status about debug/encryption"))
-	return hexchat.EAT_ALL
-
+""" Initialization"""
 def init():
 	try:
 		if os.path.isfile(PASSFILE):
-			hexchat.prnt(textPos(PASSFILE + " loaded! Decryption of incoming messages enabled."))
+			hexchat.prnt(textPos(PASSFILE + " loaded!"))
 			hexchat.hook_command('', send)
 			hexchat.hook_command('enc', enc,help="For help use the command /enc")
 			hexchat.hook_print('Private Message to Dialog', receive)	
